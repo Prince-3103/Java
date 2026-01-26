3bb070e357c16384640de2ea5b546f700887dd6e
Prince-3103 <p2577430@gmail.com>
2026-01-26 18:42:47 +0530

    Runs first program of java

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+
+        //THIS IS HOW SHOW OUTPUT
+        
+        System.out.println("Hello");
+        System.out.print("I love Pizza\n");
+        System.out.print("Can you buy me one?");
+    }
+}
