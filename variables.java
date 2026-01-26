f951939f68849f686c2ea608c2799ac1aa376477
Prince-3103 <p2577430@gmail.com>
2026-01-26 19:34:29 +0530

    Basics about variable and data types

---
+++ variables.java
@@ -0,0 +1,24 @@
+public class variables {
+    public static void main(String[] args){
+        
+        int age = 19;
+        System.out.println("Currently, I am "+ age +" year old.");
+
+        double gpa = 7.7482;
+        System.out.println("My Currently gpa is: "+gpa);
+
+        char grade = 'A';
+        System.out.println("My highest grade is: "+grade);
+
+        boolean isStudent = true;
+        System.out.println(isStudent);
+
+        String series = "Game of Thrones";
+        String name = "Prince";
+        String email = "fake123@gmail.com";
+
+        System.out.println("My favourite series is "+ series);
+        System.out.println("My name is "+name);
+        System.out.println("My email is "+email);
+    }
+}
