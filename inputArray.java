b8b99ece5975cf94e124951f9a9ee7044bcc2008
Prince-3103 <p2577430@gmail.com>
2026-02-04 00:51:48 +0530

    taking user input for string array

---
+++ inputArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class inputArray {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String[] foods;
+        int size;
+
+        System.out.print("What # of food you want?: ");
+        size = scanner.nextInt();
+        scanner.nextLine();
+
+        foods = new String[size];
+
+        for(int i=0; i<foods.length; i++){
+            System.out.print("Enter the food: ");
+            foods[i] = scanner.nextLine();
+        }
+
+        System.out.println("Your food list: ");
+        for(String food : foods){
+            System.out.print(food +" ");
+        }
+    }
+}
