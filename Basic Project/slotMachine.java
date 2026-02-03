da34461fed361d0a905e6a0b6ae445f532dd179a
Prince-3103 <p2577430@gmail.com>
2026-02-04 01:24:45 +0530

    Project of slot machine taken variable and display hello msg

---
+++ Basic Project/slotMachine.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class slotMachine {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int balance = 100;
+        int bet;
+        int payout;
+        String[] row;
+
+        // Display Welcome msg
+        System.out.println("*************************");
+        System.out.println("  Welcome to Java Slots  ");
+        System.out.println(" Symbols: 🎲 🍒 🍉 🍋 🏆 ");
+        System.out.println("*************************");
+
+        
+        scanner.close();    
+    }
+    
+}
