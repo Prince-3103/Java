c515f1c1c3bfaba59fb684225effb3dfe5828978
Prince-3103 <p2577430@gmail.com>
2026-02-05 00:32:16 +0530

    Removing comparision of first and third object its make easy to win

--- Basic Project/slotMachine.java
+++ Basic Project/slotMachine.java
@@ -112,16 +112,6 @@ public class slotMachine {
                 default -> 0;
             };
         }
-        else if(row[0].equals(row[2])){
-            return switch(row[0]){
-                case "🍋" -> bet * 2;
-                case "🍒" -> bet * 2;
-                case "🍉" -> bet * 3;
-                case "🎲" -> bet * 4;
-                case "🏆" -> bet * 5;
-                default -> 0;
-            };
-        }
         return 0;
     }
 
