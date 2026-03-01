09ff26b972ba27ccc0a0047c0da410cc701bb8c0
Prince-3103 <p2577430@gmail.com>
2026-03-02 00:50:14 +0530

    Adding method of returning book and creating object of book class in main function

--- object-oriented-programming/library.java
+++ object-oriented-programming/library.java
@@ -1,4 +1,5 @@
-class book{
+
+class Book{
     static int totalNoOfBooks;
     String title;
     String author;
@@ -12,13 +13,13 @@ class book{
         totalNoOfBooks++;
     }
 
-    book(int uniqueNum, String title, String author){
+    Book(int uniqueNum, String title, String author){
         this.uniqueNum = uniqueNum;
         this.title = title;
         this.author = author;
     }
 
-    book(int uniqueNum){    // Giving default value to title and author unique num important
+    Book(int uniqueNum){    // Giving default value to title and author unique num important
         this(uniqueNum, "Unknown", "Unknown");
     }
 
@@ -35,10 +36,28 @@ class book{
             System.out.println("Enjoy your book");
         }
     }
+
+    void returnBook(){
+        if(isBorrowed){
+            this.isBorrowed = false;
+            System.out.println("Thanks for returning book, Please leave a reviesw");
+        }
+        else{
+            System.out.println("This book already in library.");
+        }
+    }
 }
 
 public class library{
     public static void main(String[] args) {
-        
+        Book asowaf = new Book(1, "Winds of Winter", "George RR Martin");
+        Book myBook = new Book(2);
+
+        System.out.println(Book.getTotalNoOfBooks());
+        asowaf.borrowBook();
+        myBook.borrowBook();
+        asowaf.borrowBook();
+        asowaf.returnBook();
+        asowaf.returnBook();
     }
 }
