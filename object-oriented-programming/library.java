2f6a79a0c94836272c93ec2f5ed2305fddd71dd1
Prince-3103 <p2577430@gmail.com>
2026-03-02 00:34:17 +0530

    Creating a constructor for default value of unique number

--- object-oriented-programming/library.java
+++ object-oriented-programming/library.java
@@ -1,8 +1,25 @@
 class book{
     static int totalNoOfBooks;
-    string tittle;
-    string author;
+    String title;
+    String author;
     int uniqueNum;
+
+    static{
+        totalNoOfBooks = 0;
+    }
+    {
+        totalNoOfBooks++;
+    }
+
+    book(int uniqueNum, String title, String author){
+        this.uniqueNum = uniqueNum;
+        this.title = title;
+        this.author = author;
+    }
+
+    book(int uniqueNum){    // Giving default value to title and author unique num important
+        this(uniqueNum, "Unknown", "Unknown");
+    }
 }
 
 public class library{
