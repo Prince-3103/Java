8f2622c73b2ea0c144142e8d42d67b001eb64498
Prince-3103 <p2577430@gmail.com>
2026-03-05 18:02:43 +0530

    Using switch statement in week days

---
+++ daySwitch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class daySwitch {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter day: ");
+        String day = scanner.nextLine().toLowerCase();
+
+        switch(day) {
+            case "monday":
+                System.out.println("Today is Monday (working day).");
+                break;
+            case "tuesday":
+                System.out.println("Today is Tuesday (working day)");
+                break;
+            case "wednesday":
+                System.out.println("Today is Wednesday (working day)");
+                break;
+            case "thursday":
+                System.out.println("Today is Thursday (working day)");
+                break;
+            case "friday":
+                System.out.println("Today is Friday (working day)");
+                break;
+            case "saturday":
+                System.out.println("Today is Saturday (Weekend)");
+                break;
+            case "sunday":
+                System.out.println("Today is Sunday (Weekend)");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+
+        }
+        scanner.close();
+    }    
+}
