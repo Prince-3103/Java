6b6a72bfad8e9c16c3d5a1823ef36e90740e945e
Prince-3103 <p2577430@gmail.com>
2026-03-15 01:25:05 +0530

    Making 2 objects in one giving the arguments but in other default

--- This-Super/SuperMethod.java
+++ This-Super/SuperMethod.java
@@ -1,17 +1,24 @@
+// super() is a keyword used to refer to the parent class (superclass).
+// super always presnt in constructor but we cant see it like john cena
+
 class A{
     public A(){
+        super();
         System.out.println("In A");
     }
     public A(int x){
+        super();
         System.out.println("In A int");
     }
 }
 
 class B extends A{
     public B(){     // Default constructor
+        super(7);
         System.out.println("In B");
     }
     public B(int n){
+        super(n);
         System.out.println("In B int");
     }
 }
@@ -27,5 +34,17 @@ public class SuperMethod {
         // Output  
         // In A.  // Why not giving this as int?????
         // In B int
+
+        ///////// FROM HERE USING SUPER() METHOD
+
+        // B obj = new B(10); 
+        // Output  
+        // In A int
+        // In B int
+
+        B obj = new B();
+        // Output  
+        // In A int
+        // In B
     }
 }
